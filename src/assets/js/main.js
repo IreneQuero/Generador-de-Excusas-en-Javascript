25831e6cd37f69edd3bed1823521c1c1134a3e48
IreneQuero <queroirene@gmail.com>
2022-11-10 20:38:20 +0000

    excuse generator cambios

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,14 +3,18 @@
         let excuse = document.querySelector("#excuse");
         let generateExcuse = () => {
             let who = ['the dog','my granma','his turtle','my bird'];
-            let what = ['eat','pissed','crushed','broked'];
+            let action = ['eat','pissed','crushed','broked'];
+            let what = ['my homework', 'the keys', 'the car'];
             let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
             let whoIndx = Math.floor(Math.random() * who.length);
+            let actionIndx = Math.floor(Math.random() * action.length);
             let whatIndx = Math.floor(Math.random() * what.length);
             let whenIndex = Math.floor(Math.random() * when.length);
             let result =
                 who[whoIndx] +
+                " " +
+                action[actionIndx] +
                 " " +
                 what[whatIndx] +
                 " " +
